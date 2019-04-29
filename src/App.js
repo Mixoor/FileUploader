808c9dfbf5c0824fe3d221de72d1bfbf0c2b074f
Chady Mbarki <mbarkicchady@gmail.com>
2019-04-29 07:43:02 +0100

    initialization

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import FileComponent from './Component/FileComponent/FileComponent';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+      constructor(props){
+        super(props);
+        this.state={
+          files:[]
+        };
+
+        this.onChangeFile= this.onChangeFile.bind(this);
+      }
+        onChangeFile(e,f){
+          e.preventDefault();
+          let ffs = f || e.target.files;
+          this.setState({files:ffs});
+        }
+
+   
+   
+
+
+
+        render() {
+          return (
+            <div className="App">
+              <FileComponent options={{accept:"image/png"}} files={this.state.files} 
+             multiple={true} 
+              onFileChange={this.onChangeFile}/>
+            </div>
+          );
+        }
 }
 
 export default App;
